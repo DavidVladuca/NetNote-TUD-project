1df0f3f0dc524672a7b58ef0adae332c46bf81ab
Vladuca David <davidvladuca@gmail.com>
2024-11-28 19:30:58 +0100

    Revert "Creating the classes Collection, Server and CollectionTest and NoteTest, updating Note class"
    This reverts commit 995bb358b339ec9b5e2579eaa78832256257726d.

--- commons/src/main/java/commons/Note.java
+++ commons/src/main/java/commons/Note.java
@@ -1,77 +1,46 @@
 package commons;
 
-import jakarta.persistence.*;
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Entity
 public class Note {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int noteId;
-
+    public long id;
     @ManyToOne
-    @JoinColumn(name = "collection_id", nullable = false)
-    private Collection collection;
+    @JoinColumn(name = "user_id") // Adjust the column name as needed
+    private User user;
+    public String contents;
 
-    @Column(nullable = false) // Ensure that the title is mandatory in the database
-    private String title;
-
-    @Column(nullable = false) // Ensure that the title is mandatory in the database
-    private String body;
-
-    @ElementCollection
-    private List<String> tags;
-
-    // Protected no-arg constructor for JPA and object mappers
-    protected Note() {}
-
-    /**
-     * Constructor for the Note class.
-     * @param title - Title of the note.
-     * @param body - Content/body of the note.
-     * @param collection - The collection the note is part of.
-     */
-    public Note(String title, String body, Collection collection) {
-        this.title = title;
-        this.body = body;
-        this.collection = collection;
-        this.tags = new ArrayList<>(); // declare it empty for now
+    @SuppressWarnings("unused")
+    private Note() {
+        // for object mappers
     }
 
     /**
-     * Getter for the noteId
-     * @return integer value of the noteId
+     * Constructor for the Note Class.
+     * @param contents - String representing the content of the note.
      */
-    public int getNoteId() {
-        return noteId;
-    }
-
-    /**
-     * Setter for the noteId
-     * @param noteId - the value of the ID that we want to set
-     */
-    public void setNoteId(int noteId) {
-        this.noteId = noteId;
+    public Note(String contents) {
+        this.contents = contents;
     }
 
     /**
      * Equals method for the Note class.
-     * Returns true if obj is also a Note and has the same ID.
+     * Returns true if obj is also a Note and has the same parameters.
      * Else returns false
      * @param obj - Object we compare with
-     * @return boolean whether they are equal or not based on the ID
+     * @return boolean whether they are equal or not
      */
     @Override
     public boolean equals(Object obj) {
-        if(this == obj)
-            return true;
-        if(obj == null || getClass() != obj.getClass())
-            return false;
-        Note note = (Note) obj;
-        return noteId == note.noteId;
+        return EqualsBuilder.reflectionEquals(this, obj);
     }
 
     /**
@@ -80,7 +49,7 @@ public class Note {
      */
     @Override
     public int hashCode() {
-        return Integer.hashCode(noteId);
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 
     /**
@@ -89,17 +58,6 @@ public class Note {
      */
     @Override
     public String toString() {
-        StringBuilder tagsString = new StringBuilder();
-        for(int i = 0; i < tags.size(); i++) {
-            tagsString.append(tags.get(i));
-            if(i < tags.size() - 1)
-                tagsString.append(", ");
-        }
-        return "Note:\n" +
-                "Note ID: " + noteId + "\n" +
-                "Collection ID: " + collection.getCollectionId() + "\n" +
-                "Title: " + title + "\n" +
-                "Body:\n" + body + "\n" +
-                "Tags: " + tagsString + "\n";
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
     }
 }
