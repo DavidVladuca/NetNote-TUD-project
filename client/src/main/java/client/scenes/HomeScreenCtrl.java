db4d4bbf8e4f17a8144a79a938da70d70e1b2a31
Tomas Holinka <T.Holinka@student.tudelft.nl>
2024-11-28 01:02:45 +0100

    Replaced lambda function with a regulata changeValue function

--- client/src/main/java/client/scenes/HomeScreenCtrl.java
+++ client/src/main/java/client/scenes/HomeScreenCtrl.java
@@ -1,6 +1,8 @@
 package client.scenes;
 
 import client.HomeScreen;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -21,8 +23,8 @@ public class HomeScreenCtrl {
     public TextField searchF;
     public WebView markDownOutput;
 
-    private final Parser parser = Parser.builder().build();
-    private final HtmlRenderer renderer = HtmlRenderer.builder().build();
+    public final Parser parser = Parser.builder().build();
+    public final HtmlRenderer renderer = HtmlRenderer.builder().build();
 
     /**
      * This method initializes the controller
@@ -30,13 +32,17 @@ public class HomeScreenCtrl {
      */
     @FXML
     public void initialize() {
-        noteBodyF.textProperty().addListener((observable, oldValue, newValue) -> {
-            // MD -> HTML
-            String html = renderer.render(parser.parse(newValue));
-            // WebView is updated based on the HTML file
-            markDownOutput.getEngine().loadContent(html);
+        noteBodyF.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                // MD -> HTML
+                String html = renderer.render(parser.parse(newValue));
+                // WebView is updated based on the HTML file
+                markDownOutput.getEngine().loadContent(html);
+            }
         });
     }
+
 
     /**
      * Utility function used to locate resources within applications filepath
