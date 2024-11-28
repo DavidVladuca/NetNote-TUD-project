74d2dafaad8b7dc6b1e81628b1fba997f2520946
Tomas Holinka <T.Holinka@student.tudelft.nl>
2024-11-28 23:49:55 +0100

    Added proper testing for the Server class.

---
+++ commons/src/test/java/commons/ServerTest.java
@@ -0,0 +1,89 @@
+package commons;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class ServerTest {
+    private Collection collection1;
+    private Server server1;
+    private Server server2;
+
+    @BeforeEach
+    public void setUp() {
+        server1 = new Server();
+        server2 = new Server();
+        collection1 = new Collection(server1, "Test Collection 1");
+    }
+
+    @Test
+    public void getterAndSetterTest() {
+        server1.setServerId(3);
+        int expected = 3;
+        int actual = server1.getServerId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void equalsSameParametersTest() {
+        server1.setServerId(3);
+        server2.setServerId(3);
+        server1.addCollection(collection1);
+        server2.addCollection(collection1);
+        assertEquals(server1, server2);
+    }
+
+    @Test
+    public void equalsSameTest() {
+        server1.setServerId(3);
+        server1.addCollection(collection1);
+        assertEquals(server1, server1);
+    }
+
+    @Test
+    public void equalsNullTest() {
+        server1.setServerId(3);
+        server2 = null;
+        server1.addCollection(collection1);
+        assertNotEquals(server1, server2);
+    }
+
+    @Test
+    public void equalsHashCodeSameParametersTest() {
+        server1.setServerId(3);
+        server2.setServerId(3);
+        server1.addCollection(collection1);
+        server2.addCollection(collection1);
+        assertEquals(server1.hashCode(), server2.hashCode());
+    }
+
+    @Test
+    public void toStringTest() {
+        server1.setServerId(3);
+        server1.addCollection(collection1);
+        server1.addCollection(collection1);
+        String actual = server1.toString();
+        String expected = "Server:\n" +
+                "Server ID: 3\n" +
+                "Collections:\n" +
+                "\n" +
+                "Collection:\n" +
+                "Collection ID: 0\n" +
+                "Server ID: 3\n" +
+                "Collection Title: Test Collection 1\n" +
+                "Notes:\n" +
+                "\n" +
+                "No Notes\n" +
+                "#########################\n" +
+                "Collection:\n" +
+                "Collection ID: 0\n" +
+                "Server ID: 3\n" +
+                "Collection Title: Test Collection 1\n" +
+                "Notes:\n" +
+                "\n" +
+                "No Notes";
+        assertEquals(expected, actual);
+    }
+}
