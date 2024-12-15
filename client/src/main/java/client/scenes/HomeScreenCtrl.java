7cf418f27285a6bd3a35ea6249d1751b57de98e0
David Vlăducă <d.vladuca@student.tudelft.nl>
2024-12-15 18:38:52 +0100

    Update HomeScreenCtrl.java

--- client/src/main/java/client/scenes/HomeScreenCtrl.java
+++ client/src/main/java/client/scenes/HomeScreenCtrl.java
@@ -28,7 +28,7 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
+import java.io.IOException;
 import commons.Collection;
 import commons.Server;
 
