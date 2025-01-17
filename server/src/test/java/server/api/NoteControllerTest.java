d05ba48e9ff1e5d0974d1e7b7623ad87025c6ec8
Tomas Holinka <T.Holinka@student.tudelft.nl>
2025-01-17 13:36:39 +0100

    Added testing for some methods associated with notes in Note Controller

--- server/src/test/java/server/api/NoteControllerTest.java
+++ server/src/test/java/server/api/NoteControllerTest.java
@@ -1,6 +1,7 @@
 package server.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import commons.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
@@ -18,6 +19,10 @@ import commons.Collection;
 import commons.Note;
 import commons.Server;
 import server.database.TagRepository;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.mockito.Mockito.when;
 
@@ -85,6 +90,121 @@ public class NoteControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Test Note"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.body").value("Test Note"));
+    }
+
+    /**
+     * Tests the update note endpoint, mocking the database behaviour
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testUpdateNoteSuccess() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Note updated = new Note();
+        updated.setNoteId(1L);
+        updated.setTitle("Test Note updated");
+        updated.setBody("Test Note updated");
+
+        when(noteRepository.findById(1L)).thenReturn(Optional.of(note));
+        when(noteRepository.save(updated)).thenReturn(updated);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/notes/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.noteId").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Test Note updated"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.body").value("Test Note updated"));
+    }
+
+    /**
+     * Tests the update note endpoint, when the ID is less than 1
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testUpdateNoteBadId() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Note badId = new Note();
+        badId.setNoteId(0L);
+        badId.setTitle("Test Note");
+        badId.setBody("Test Note");
+
+        Note updated = new Note();
+        updated.setNoteId(0L);
+        updated.setTitle("Test Note");
+        updated.setBody("Test Note");
+
+        when(noteRepository.findById(0L)).thenReturn(Optional.of(badId));
+        when(noteRepository.save(updated)).thenReturn(updated);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/notes/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    /**
+     * Tests the delete endpoint, expect 404 Not Found after deletion
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testDeleteNoteSuccess() throws Exception {
+        when(noteRepository.existsById(1L)).thenReturn(true);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/notes/delete/{id}", 1L))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    /**
+     * Tests the delete endpoint with bad id
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testDeleteNoteBadId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/notes/delete/{id}", 0L))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Tests the get endpoint with a valid id
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetNoteByIdSuccess() throws Exception {
+        when(noteRepository.findById(1L)).thenReturn(Optional.of(note));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/notes/{id}", 1L))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.noteId").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Test Note"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.body").value("Test Note"));
+    }
+
+    /**
+     * Tests the getter for the tags of a note by its ID
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetTagsByNoteId() throws Exception {
+        Note tagNote = new Note();
+        tagNote.setNoteId(20L);
+        Tag tag1 = new Tag("Tag1");
+        Tag tag2 = new Tag("Tag2");
+        Set<Tag> tags = new HashSet<>(Set.of(tag1, tag2));
+        note.setTags(tags);
+
+        when(noteRepository.findById(20L)).thenReturn(Optional.of(note));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/notes/{id}/tags", 20L))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2)) // Check number of tags
+                .andExpect(MockMvcResultMatchers.jsonPath("$[?(@.name == 'Tag1')]").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[?(@.name == 'Tag2')]").exists());
     }
 
     @Test
